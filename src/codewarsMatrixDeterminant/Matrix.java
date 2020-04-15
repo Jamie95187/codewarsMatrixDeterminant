cd7dca0e5f8b8602a45fdca470f362f247d377c6
Jamie95187 <jamiewong63@googlemail.com>
2020-04-15 06:11:52 +0100

    refactor into 2 methods and made method recursive

--- src/codewarsMatrixDeterminant/Matrix.java
+++ src/codewarsMatrixDeterminant/Matrix.java
@@ -13,30 +13,71 @@ package codewarsMatrixDeterminant;
 public class Matrix {
 	
 	static int determinant(int[][] matrix) {
-		if(matrix[0].length == 1) {
-			return matrix[0][0];
-		} else if (matrix[0].length == 2) {
-			return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-		}
-		if(matrix[0].length == 3) {
-			return matrix[0][0] * ((matrix[1][1] * matrix[2][2]) - (matrix[2][1] * matrix[1][2])) -
-				   matrix[1][0] * ((matrix[0][1] * matrix[2][2]) - (matrix[2][1] * matrix[0][2])) +
-				   matrix[2][0] * ((matrix[0][1] * matrix[1][2]) - (matrix[1][1] * matrix[0][2]));
-		}
-		return matrix[0][0] * (matrix[1][1] * (matrix[2][2] * matrix[3][3] - matrix[3][2] * matrix[2][3]) - 
-							   matrix[2][1] * (matrix[1][2] * matrix[3][3] - matrix[3][2] * matrix[1][3]) +
-							   matrix[3][1] * (matrix[1][2] * matrix[2][3] - matrix[2][2] * matrix[1][3]))
-			- (matrix[1][0] * (matrix[0][1] * (matrix[2][2] * matrix[3][3] - matrix[3][2] * matrix[2][3]) -
-							   matrix[2][1] * (matrix[0][2] * matrix[3][3] - matrix[3][2] * matrix[0][3]) +
-							   matrix[3][1] * (matrix[0][2] * matrix[2][3] - matrix[2][2] * matrix[0][3])))
-			+ (matrix[2][0] * (matrix[0][1] * (matrix[1][2] * matrix[3][3] - matrix[3][2] * matrix[1][3]) -
-							   matrix[1][1] * (matrix[0][2] * matrix[3][3] - matrix[3][2] * matrix[0][3]) + 
-							   matrix[3][1] * (matrix[0][2] * matrix[1][3] - matrix[1][2] * matrix[0][3])))
-			- (matrix[3][0] * (matrix[0][1] * (matrix[1][2] * matrix[2][3] - matrix[2][2] * matrix[1][3]) -
-							   matrix[1][1] * (matrix[0][2] * matrix[2][3] - matrix[2][2] * matrix[0][3]) +
-							   matrix[2][1] * (matrix[0][2] * matrix[1][3] - matrix[1][2] * matrix[0][3]))
-			  );
-
+		return determinantOfMatrix(matrix, matrix.length);
 	}
+	
+	// Method to get cofactor of mat[p[[q] in temp[][]
+	// n is the current dimension of the mat[][]
+	
+	static void getCofactors(int[][] mat, int temp[][], int p, int q, int n) {
+		int i = 0, j = 0;
+		
+		// Looping through the element of the matrix
+		
+		for (int row = 0; row < n; row++) {
+			
+			for (int col = 0; col < n; col++) {
+				
+				// Copying into temporary matrix
+				// only those elements which are
+				// not in given row and column
+				
+				if (row != p && col != q) {
+					temp[i][j++] = mat[row][col];	
+				
+					// row is filled, so increase 
+					// row index and reset col
+					// index
+					if (j == n - 1) {
+						j = 0;
+						i++;
+					}
+				}
+			}
+		}
+	}
+	
+	// recursive method to find determinant where n is the current dimension of the matrix
+	// mat[][]
+	
+	static int determinantOfMatrix(int[][] mat, int n) {
+		int determinant = 0;
+		
+		// Base case when single element
+		if (n == 1) {
+			return mat[0][0];
+		}
+		
+		// Store cofactors
+		int temp[][] = new int[mat[0].length][mat[0].length];
+		
+		// sign multiplier
+		int sign = 1;
+
+		// iterate each element of the first row
+		for (int f = 0; f < n; f++) {
+			// getting cofactors of matrix [0][f]
+			getCofactors(mat, temp, 0, f, n);
+			determinant += sign * mat[0][f] 
+					* determinantOfMatrix(temp, n - 1);
+			
+			// terms are to be added with alternate signs
+			sign = -sign;
+		}
+		
+		return determinant;
+	}
+	
+	
 	
 }
